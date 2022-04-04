e66c083d0d5f45b412e2ba37ef1d0a22a23d9d98
Nick Chmela <nickchmela@gmail.com>
2022-04-04 11:02:51 -0400

    added users and comments

--- src/index.js
+++ src/index.js
@@ -2,12 +2,60 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put("/users/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 app.listen(process.env.PORT, () =>
