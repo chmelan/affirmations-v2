771b148ed5d2de96749a29646b2bc68230c2ea72
Nick Chmela <nickchmela@gmail.com>
2022-04-04 10:38:18 -0400

    added express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello ever running Node.js project.");
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
